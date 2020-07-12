e16cac9b4114c6b7ac8554f1d9076222a2a8ede0
Eunju-Han <eunju.han1@gmail.com>
2020-07-11 22:42:05 -0400

    Intro to React - ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
+// ReactDOM.render()
+// - uses a DOM node in your HTML to replace it with JSX.
+// - can be used multiple times across your application
+// - You can use it to bootstrap
+// -- simple JSX syntax,
+// -- a React component,
+// -- multiple React components,
+// -- or an entire application.
+
+//ReactDOM.render has two arguments
+// first: This is for rendering the JSX
+// second: the place where the React application hooks into your HTML
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
