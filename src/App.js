04e50a9f86d44d430c960e4a0567348756b16d13
Eunju-Han <eunju.han1@gmail.com>
2020-08-16 00:20:56 -0400

    Basics - Component Declarations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import { func } from "prop-types";
 
 const list = [
   {
@@ -122,55 +123,172 @@ class App extends Component {
 // creates an instance of the component
 export default App;
 
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props;
-    return (
-      <form>
-        {children}
-        <input type="text" value={value} onChange={onChange} />
-      </form>
-    );
-  }
-}
+// // Refactoring ES6 Class Compnents -> Functional Stateless Components
+// // as you don’t need local state or component lifecycle methods
+// // Step 1
+// // The props are accessible in the function signature,
+// // and the return value is JSX
+// function Search(props) {
+//   const { value, onChange, children } = props;
+//   return (
+//     <form>
+//       {children}
+//       <input type="text" value={value} onChange={onChange} />
+//     </form>
+//   );
+// }
 
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item => {
-          return (
-            <div key={item.objectID}>
-              {/* Make sure that the key attribute is a stable identifier. */}
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <Button onClick={() => onDismiss(item.objectID)}>
-                  Dismiss
-                </Button>
-              </span>
-            </div>
-          );
-        })}
+// // Refactoring ES6 Class Compnents -> Functional Stateless Components
+// // Step 2
+// // The best practice is to use it in the function signature
+// // to destructure the props
+// function Search({ value, onChange, children }) {
+//   return (
+//     <form>
+//       {children}
+//       <input type="text" value={value} onChange={onChange} />
+//     </form>
+//   );
+// }
+
+// // Refactoring ES6 Class Compnents -> Functional Stateless Components
+// // Step 3
+// // ES6 arrow functions that makes fuctions concise and also removes the block body of the function.
+// // In a concise body, an implicit return is attached, removes the return statement.
+// const Search = ({ value, onChange, children }) => (
+//   <form>
+//     {children}
+//     <input type="text" value={value} onChange={onChange} />
+//   </form>
+// );
+
+// Refactoring ES6 Class Compnents -> Functional Stateless Components
+// Step 4
+// props as input and JSX as output, and you could do something in between
+const Search = ({ value, onChange, children }) => {
+  // do something
+  return (
+    <form>
+      {children}
+      <input type="text" value={value} onChange={onChange} />
+    </form>
+  );
+};
+
+// class Table extends Component {
+//   render() {
+//     const { list, pattern, onDismiss } = this.props;
+//     return (
+//       <div>
+//         {list.filter(isSearched(pattern)).map(item => {
+//           return (
+//             <div key={item.objectID}>
+//               {/* Make sure that the key attribute is a stable identifier. */}
+//               <span>
+//                 <a href={item.url}>{item.title}</a>
+//               </span>
+//               <span>{item.author}</span>
+//               <span>{item.num_comments}</span>
+//               <span>{item.points}</span>
+//               <span>
+//                 <Button onClick={() => onDismiss(item.objectID)}>
+//                   Dismiss
+//                 </Button>
+//               </span>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     );
+//   }
+// }
+
+// // Step1
+// function Table(props) {
+//   const { list, pattern, onDismiss } = props;
+//   return (
+//     <div>
+//       {list.filter(isSearched(pattern)).map(item => {
+//         return (
+//           <div key={item.objectID}>
+//             {/* Make sure that the key attribute is a stable identifier. */}
+//             <span>
+//               <a href={item.url}>{item.title}</a>
+//             </span>
+//             <span>{item.author}</span>
+//             <span>{item.num_comments}</span>
+//             <span>{item.points}</span>
+//             <span>
+//               <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
+//             </span>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+// Step2 and Step3 ES6 arrow functions
+const Table = ({ list, pattern, onDismiss }) => (
+  <div>
+    {list.filter(isSearched(pattern)).map(item => (
+      <div key={item.objectID}>
+        {/* Make sure that the key attribute is a stable identifier. */}
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <span>
+          <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
+        </span>
       </div>
-    );
-  }
-}
+    ))}
+  </div>
+);
 
-class Button extends Component {
-  render() {
-    // The className will be a default value of empty string instead of undefined.
-    const { onClick, className = "", children } = this.props;
+// // Step4 - some login between input and output
+// const Table = ({ list, pattern, onDismiss }) => {
+//   //do someting
+//   return (
+//     <div>
+//       {list.filter(isSearched(pattern)).map(item => {
+//         return (
+//           <div key={item.objectID}>
+//             {/* Make sure that the key attribute is a stable identifier. */}
+//             <span>
+//               <a href={item.url}>{item.title}</a>
+//             </span>
+//             <span>{item.author}</span>
+//             <span>{item.num_comments}</span>
+//             <span>{item.points}</span>
+//             <span>
+//               <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
+//             </span>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// };
 
-    return (
-      <button onClick={onClick} className={className} type="button">
-        {children}
-      </button>
-    );
-  }
-}
+// class Button extends Component {
+//   render() {
+//     // The className will be a default value of empty string instead of undefined.
+//     const { onClick, className = "", children } = this.props;
+
+//     return (
+//       <button onClick={onClick} className={className} type="button">
+//         {children}
+//       </button>
+//     );
+//   }
+// }
+
+// Refactoring ES6 class component -> functional stateless component
+const Button = ({ onClick, className = "", children }) => (
+  <button onClick={onClick} className={className} type="button">
+    {children}
+  </button>
+);
